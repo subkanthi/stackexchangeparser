cd3f89d5378e002f030d6e46e87019799eac0b15
Kanthi Subramanian <subkanthi@gmail.com>
2018-06-02 19:42:08 -0400

    Added arguments cassandra ipAddress, Keyspace so that it can be passed to Cassandra connector

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import argparse;
 import sys;
 from XmlParser import XmlParser;
 from connector.SparkConnector import  SparkConnector;
+from connector.CassandraConnector import CassandraConnector;
 
 
 
@@ -10,7 +11,8 @@ def main():
     parser.add_argument('--file',
                         help='pass the file to parse the xml')
     parser.add_argument('--delimiter', help='delimiter in the file')
-    parser.add_argument('--cassandraDb', help='Cassandra DB IP address')
+    parser.add_argument('--cassandraIP', help='Cassandra DB IP address')
+    parser.add_argument('--cassandraKeyspace', help='Cassandra Keyspace')
 
 
     args = parser.parse_args()
@@ -24,6 +26,15 @@ def main():
     list = xmlParser.parseXml()
     #print(list.count())
 
+    cassandraIP = args.cassandraIP
+    cassandraKeyspace = args.cassandraKeyspace
+
+    if cassandraIP is not None and cassandraKeyspace is not None:
+        connector = CassandraConnector(cassandraIP, cassandraKeyspace)
+    else:
+        print(args.file)
+        sys.exit(0)
+        
     SparkConnector('s3n://sparkapache/Posts.xml')
 
 main()
--- connector/CassandraConnector.py
+++ connector/CassandraConnector.py
@@ -2,6 +2,6 @@ from cassandra.cluster import Cluster
 
 class CassandraConnector:
 
-    def __init__(self):
+    def __init__(self, ipAddress, keySpace):
         print("CassandraConnector constructor")
 
